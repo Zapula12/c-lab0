cebef2d1d6cfbadc154c90d7d85e0ff484910d22
Zapula12 <70684710+Zapula12@users.noreply.github.com>
2020-09-03 18:39:06 +0300

    Update kvadrat.c
    Добавление файла kvadrat.c

--- kvadrat.c
+++ kvadrat.c
@@ -1,35 +1,22 @@
 #include <stdio.h>
 #include <math.h>
-#include <stdlib.h>
-
-
-
-float f(int a, int b, int c)  
-{
-  double d,x1,x2;
-  d =(b*b)-4*a*c;
-  if (d>0)
-  {
-    x1=(-b+sqrt(d))/(2*a);
-    x2=(-b-sqrt(d))/(2*a);
-    printf("x1=%f",x1);
-    printf("x2=%f",x2);
-  }
-  if(d<0)
-  {
-    printf("net korney");
-  }
-  if(d==0)
-  {
-    x1=(-b)/(2*a);
-    printf("x1=%f",x1);
-  }
-}
-
-int main()
-{
-  int a,b,c;
-  scan (&a,&b,&c);
-  f(a,b,c);
-  return 0;
+ 
+main() {
+    float a,b,c,d,x1,x2;
+    printf("a="); scanf("%f",&a);
+    printf("b="); scanf("%f",&b);
+    printf("c="); scanf("%f",&c);
+    d = b*b - 4*a*c;
+    if (d>0) {
+        x1 = (-b + sqrt(d)) / (2*a);
+        x2 = (-b - sqrt(d)) / (2*a);
+        printf("x1=%.2f; x2=%.2f", x1, x2);
+    }
+    else
+        if (d = 0) {
+            x1 = -b / (2*a);
+            printf("x1=%.2f; x2=%.2f", x1, x2);
+        }
+        else printf("Корней нет.");
+    printf("\n");
 }
